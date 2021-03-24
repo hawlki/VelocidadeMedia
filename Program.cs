9f257a1221687a1b35c80de671548ca3f588118a
israel lopes <israel.lopes2710@gmail.com>
2021-03-24 16:01:16 -0300

    finaliza o programa

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace velocidadeMedia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.Beep();
+
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine("Etec Adolpho Berezin");
+            Console.WriteLine("Feito por Israel Camilo Lopes");
+            Console.WriteLine("1I3");
+            Console.WriteLine("-----------------------------");
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Insira os dados a seguir:");
+            Console.Write("Distância em metros: ");
+            String D = Console.ReadLine();
+            Console.Write("Tempo em segundos: ");
+            String T = Console.ReadLine();
+
+            Double distancia = Convert.ToDouble(D);
+            Double Tempo = Convert.ToDouble(T);
+            Double velocidadeMedia = distancia / Tempo;
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine($"A velocidade média é {velocidadeMedia} m/s");
+
+            Console.ResetColor();
+            Console.WriteLine("Toque em uma tecla para limpar sua tela");
+            Console.ReadKey();
+
+            Console.Clear();
+
+
+        }
+    }
+}
